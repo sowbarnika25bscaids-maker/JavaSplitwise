fbace0a8ded9e7456232565746f0e73e0d03fd99
Sowbarnika <sowbarnika.25bscaids@kprcas.ac.in>
2026-08-06 18:30:17 +0530

    Complete Section 1.2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         System.out.println("Track shared expenses with friends.");
         System.out.println("Ready. More features coming in the next lessons.");
 
+        double totalAmount = 500.0;
+        int numFriends = 3;
+
+        System.out.println("Total Amount: " + totalAmount);
+        System.out.println("Number of Friends: " + numFriends);
+
     }
 
 }

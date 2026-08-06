c28d88055d0cda8341944eea832ec53429303d7c
Sowbarnika <sowbarnika.25bscaids@kprcas.ac.in>
2026-08-06 18:30:37 +0530

    Complete Section 1.3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String payerName = "Riya";
+        double totalAmount = 500.0;
+        int numFriends = 3;
+
         System.out.println("=== Splitwise Console ===");
         System.out.println("Track shared expenses with friends.");
         System.out.println("Ready. More features coming in the next lessons.");
 
-        double totalAmount = 500.0;
-        int numFriends = 3;
-
+        System.out.println("Payer Name: " + payerName);
         System.out.println("Total Amount: " + totalAmount);
         System.out.println("Number of Friends: " + numFriends);
 

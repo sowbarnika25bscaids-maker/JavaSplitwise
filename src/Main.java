38c84e8a9c83b3d3ad69b393d439f5460ff50ca7
Sowbarnika <sowbarnika.25bscaids@kprcas.ac.in>
2026-08-06 18:22:37 +0530

    Complete Section 1.1 - Our First Program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Splitwise Console ===");
+        System.out.println("Track shared expenses with friends.");
+        System.out.println("Ready. More features coming in the next lessons.");
+
+    }
+
+}
